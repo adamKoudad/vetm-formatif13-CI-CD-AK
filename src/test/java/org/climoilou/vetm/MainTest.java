14832094421b7ad8e8ccbe7dcdf2db0339f99bc6
adamkoudad <2400691@cegeplimoilou.ca>
2026-05-18 12:29:20 -0400

    100% test du code

--- src/test/java/org/climoilou/vetm/MainTest.java
+++ src/test/java/org/climoilou/vetm/MainTest.java
@@ -2,6 +2,7 @@ package org.climoilou.vetm;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class MainTest {
@@ -14,5 +15,37 @@ class MainTest {
     @Test
     void foufouTest() {
         assertTrue(true);
+    }
+
+    @Test
+    void testCesarCipherSimple() {
+        String result = Main.cesarCipher("abc", 1);
+        assertEquals("bcd", result);
+    }
+
+    @Test
+    void testCesarCipherAvecEspace() {
+        String result = Main.cesarCipher("a b", 1);
+        assertEquals("b c", result);
+    }
+
+    @Test
+    void testCesarCipherBoucleAlphabet() {
+        String result = Main.cesarCipher("xyz", 2);
+        assertEquals("zab", result);
+    }
+
+    @Test
+    void testCesarDecipher() {
+        String encoded = Main.cesarCipher("bonjour", 3);
+        String decoded = Main.cesarDecipher(encoded, 3);
+
+        assertEquals("bonjour", decoded);
+    }
+
+    @Test
+    void testOffsetSuperieur26() {
+        String result = Main.cesarCipher("abc", 27); // 27 ≡ 1
+        assertEquals("bcd", result);
     }
 }
